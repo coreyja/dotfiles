de5303da576a4e0fffb56fd6879d8e4f9d4ed131
Corey Alexander <corey@betterment.com>
2017-04-14 17:21:18 -0400

    Switch gitignore to whitelist. Add .ignore that reverses it so that ripgrep still works. Gonna move this to my home dir next

--- script/setup.sh
+++
@@ -1,9 +0,0 @@
-rsync --exclude ".git/" \
-	--exclude ".DS_Store" \
-	--exclude ".osx" \
-	--exclude "bootstrap.sh" \
-	--exclude "README.md" \
-	--exclude "LICENSE-MIT.txt" \
-	--exclude "script/" \
-	-avh --no-perms . ~;
-source ~/.bash_profile;
