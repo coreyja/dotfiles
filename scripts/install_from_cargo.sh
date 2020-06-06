021857105bfd10c7ebd39df1bb125b049dde0dbd
Corey Alexander <coreyja@gmail.com>
2020-06-06 19:00:22 -0400

    We do still want to update the plugin though

--- scripts/install_from_cargo.sh
+++ scripts/install_from_cargo.sh
@@ -15,4 +15,4 @@ install_from_cargo cargo-update
 
 # I want word splitting cause each word is a different package to update
 # shellcheck disable=SC2046
-cargo install-update $(cat "$HOME/Cargofile")
+cargo install-update $(cat "$HOME/Cargofile") cargo-update
