e63911310cc96be6567b85a6c75b8246f5dac9b8
Corey Alexander <coreyja@gmail.com>
2020-06-06 18:01:19 -0400

    Fix typo and shellcheck this file

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-printf "********\nThis is the INITIAL setup script.\n********\nIf you are running this on an already setup machine stop NOW. The following is NOT written to be idempotent to any idempotency is by accident.\n\nEnter any character to agree and continue with the setup" && read -n 1
+printf "********\nThis is the INITIAL setup script.\n********\nIf you are running this on an already setup machine stop NOW. The following is NOT written to be idempotent so any idempotency is by accident.\n\nEnter any character to agree and continue with the setup" && read -rn 1
 
 pushd "$HOME"
-  printf "We are now in your home directory which is $(pwd)"
+  printf "We are now in your home directory which is %s" "$(pwd)"
 
   git init
   git remote add origin https://github.com/coreyja/dotfiles.git
