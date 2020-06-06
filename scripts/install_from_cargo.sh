f3bdcedbc4787ef04eb5fbbe46910e6e8acecd54
Corey Alexander <coreyja@gmail.com>
2020-06-06 18:58:24 -0400

    Refactor install such that the dep isn't needed in the Cargofile anymore

--- scripts/install_from_cargo.sh
+++ scripts/install_from_cargo.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+function install_from_cargo() {
+  tool=$1
+
+  bash -c "cargo install --list | grep $tool > /dev/null || cargo install $1 -- $tool"
+}
+
 while IFS= read -r tool
 do
-  bash -c "cargo install --list | grep $tool > /dev/null || cargo install $1 -- $tool"
+  install_from_cargo "$tool"
 done < <(cat "$HOME/Cargofile")
 
+install_from_cargo cargo-update
+
+# I want word splitting cause each word is a different package to update
+# shellcheck disable=SC2046
 cargo install-update $(cat "$HOME/Cargofile")
